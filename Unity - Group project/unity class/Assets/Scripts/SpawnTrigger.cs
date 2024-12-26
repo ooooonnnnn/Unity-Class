fb7ebd66714acbecd2271caaba37740d90eafa0d
OmriTafla <omritafla@gmail.com>
2024-12-26 18:32:00 +0200

    added trigger that spawns enemies
    also made EnemyWithKnife prefab

---
+++ Unity - Group project/unity class/Assets/Scripts/SpawnTrigger.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnTrigger : MonoBehaviour
+{
+    //when the player enters this trigger, some mobs spawn
+    [SerializeField] private GameObject[] mobsToSpawn;
+    [SerializeField] private Transform[] spawnAnchors;
+    private bool triggered = false;
+
+    public void Start()
+    {
+        //check mobsToSpawn and spawnAnchors correspond
+        if (mobsToSpawn.Length != spawnAnchors.Length)
+        {
+            throw new Exception("Number of mobs must equal number of spawn anchors");
+        }
+    }
+    
+    public void OnTriggerEnter(Collider other)
+    {
+        if (!triggered && other.CompareTag("Player"))
+        {
+            //spawns all mobs in their locations
+            for (int i = 0; i < mobsToSpawn.Length; i++)
+            {
+                print("maria is stupid");
+                Instantiate(mobsToSpawn[i], spawnAnchors[i]);
+            }
+            
+            //spawning only happens once
+            triggered = true;
+        }
+    }
+}
